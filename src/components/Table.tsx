6307222cb8671c8195edd594552292cd389dd0b3
mati <mati@gmail.com>
2022-07-27 10:47:28 -0300

    created home and table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Table: React.FC = () => {
+  return (
+    <>
+      <h1>Table</h1>
+    </>
+  );
+};
+export default Table;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "./../components/Table";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <Table/>
+    </>
+  );
+};
+
+export default Home;
