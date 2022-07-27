c0f91e5d932745ba99c4d7a241ba82689cc37bee
mati <mati@gmail.com>
2022-07-27 18:22:09 -0300

    chart component will display user

---
+++ src/components/Chart.tsx
@@ -0,0 +1,64 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Card } from "react-bootstrap";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+  
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label:'Income',
+      data: labels.map(() => Math.random() * 100),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+const Chart = () => {
+  return (
+    <>
+    <h3 className="pt-2">Summary</h3 >
+      <Card>
+        <Card.Body>
+          <Line options={options} data={data} className="chart" />
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default Chart;
