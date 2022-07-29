91d9e29a5ac932c83b0f924a7808fa0fc2a81aef
mati <mati@gmail.com>
2022-07-28 22:57:37 -0300

    created route that shows some example activities
    todo: install mongose and fetch local collections

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/create-activity', function(req, res, next) {
+router.get("/activities", function (req, res, next) {
+  const activity = [
+    {
+      title: "Activity 1",
+      id: 1,
+      logo: "IconProp",
+      date: "01/01/2019",
+      balanceChange: 3,
+    },
+    {
+      title: "Activity 2",
+      id: 2,
+      logo: "IconProp",
+      date: "01/01/2019",
+      balanceChange: 3,
+    },
+  ];
 
   res.json({
-    "status": "success",
-      
+    status: "success",
+    data: activity,
+  });
 });
-
 module.exports = router;
