96957f66211179b83b8ef65b9a695d6029466997
mati <mati@gmail.com>
2022-07-28 22:58:33 -0300

    showing fetched activities with redux thunk
    to do, get icons packages

--- client/src/components/RecentActivity.tsx
+++ client/src/components/RecentActivity.tsx
@@ -4,20 +4,22 @@ import { Button, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+// import * as icons  from "@fortawesome/free-regular-svg-icons";
 
+// console.log(icons)
 // icons
 // import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 // interfaces
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+// import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 // components and functions
 import { fetchActivities } from "./../store/slices/activity/index";
 
 interface Activity {
   id: number;
-  logo: IconProp;
+  logo: any;
   title: string;
   date: string;
   balanceChange: number;
@@ -26,7 +28,7 @@ interface Activity {
 const RecentActivity: React.FC = () => {
   const dispatch: any = useDispatch();
 
-  const activities: Activity[] = useSelector((state: any) => state.activity);
+  const activities: Activity[] = useSelector((state: any) => state.activity.activities.data);
 
   React.useEffect(() => {
     console.log("RecentActivity");
@@ -34,51 +36,52 @@ const RecentActivity: React.FC = () => {
     dispatch(fetchActivities());
   }, [dispatch]);
 
-  // const Activity: Activity[] ;
-
   return (
     <div className="RecentActivityContainer ">
       <div className="d-flex justify-content-between mb-2">
         <h3 className="">Recent Activity</h3>
         <Link to="/dashboard/new-activity">
           <Button variant="primary">
-            <FontAwesomeIcon icon={faPlus} /> New Transaction
+            <FontAwesomeIcon  icon={faPlus}  /> New Transaction
           </Button>
         </Link>
       </div>
 
       <Card className="py-4">
-        {Activity.slice(0, 5).map((item: any) => {
-          return (
-            <Card.Body
-              key={item.id}
-              className="d-flex justify-content-around flex-wrap"
-            >
-              <div className="d-flex justify-content-center">
-                {/* logo */}
-                <div className="logoContainer">
-                  <FontAwesomeIcon icon={item.logo} />
-                </div>
+        {activities != undefined
+          ? activities.slice(0, 5).map((item: any) => {
+              return (
+                <Card.Body
+                  key={item.id}
+                  className="d-flex justify-content-around flex-wrap"
+                >
+                  <div className="d-flex justify-content-center">
+                    {/* logo */}
+                    <div className="logoContainer">
+                      <FontAwesomeIcon icon={item.logo} />
+                    </div>
+                    <div>
+                      {/* title */}
+                      <Card.Text>{item.title}</Card.Text>
 
-                <div>
-                  {/* title */}
-                  <Card.Text>{item.title}</Card.Text>
-
-                  {/* time */}
-                  <Card.Text className="secondary-text">{item.date}</Card.Text>
-                </div>
-              </div>
-              {/* Balance change made */}
-              <Card.Text
-                className={
-                  item.balanceChange > 0 ? "success-text" : "danger-text"
-                }
-              >
-                <p>$ {item.balanceChange} USD</p>
-              </Card.Text>
-            </Card.Body>
-          );
-        })}
+                      {/* time */}
+                      <Card.Text className="secondary-text">
+                        {item.date}
+                      </Card.Text>
+                    </div>
+                  </div>
+                  {/* Balance change made */}
+                  <Card.Text
+                    className={
+                      item.balanceChange > 0 ? "success-text" : "danger-text"
+                    }
+                  >
+                    <p>$ {item.balanceChange} USD</p>
+                  </Card.Text>
+                </Card.Body>
+              );
+            })
+          : ""}
       </Card>
     </div>
   );
