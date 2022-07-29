6cbde067a4b5fa45765b9821c51e28bdbff88a5c
mati <mati@gmail.com>
2022-07-28 22:05:05 -0300

    created server file

---
+++ server/src/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/create-activity', function(req, res, next) {
+
+  res.json({
+    "status": "success",
+      
+});
+
+module.exports = router;
